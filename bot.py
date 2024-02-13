29469f90d8faf0aef4b643e6104b3027be97fb28
amir habibi <rabbitt1991x@gmail.com>
2024-02-13 12:46:09 +0330

    bot is ready

--- bot.py
+++ bot.py
@@ -1,158 +1,185 @@
 from PIL import Image
-import glob
-import os
-from telegram.ext import Updater,\
-                         CommandHandler,\
-                         MessageHandler,\
-                         Filters,\
-                         ConversationHandler
-
-# want make color filter for image
-# get image from user
-# find out pixels
-# add filter
-# send it back to user
+import os ,logging , glob
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler, CallbackContext
+from telegram import Update
+import logging
+from dotenv import load_dotenv
 
 
-def remove_image():
-    try:
-        for i in glob.glob("*.jpg"):
-            os.remove(i)
-    except Exception as delete_error:
-        print("something is wrong file not removed")
+
+class ColorFilterBot:
+    def __init__(self, token):
+        logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                           level=logging.INFO)
+        self.logger = logging.getLogger(__name__)
+        self.updater = Updater(token, use_context=True)
+        self.dp = self.updater.dispatcher
+        self.setup_handlers()
+        self.logger.info('[+]  Bot started')
+      
+
+    def setup_handlers(self):
+        self.logger.info('[+] setup handler ... ')
+        self.dp.add_handler(CommandHandler("start", self.start_command))
+        self.dp.add_handler(CommandHandler("help", self.help_command))
+        conversation_handler = ConversationHandler(
+            entry_points=[
+                CommandHandler("black_white", self.get_black_white),
+                CommandHandler("blue", self.get_blue),
+                CommandHandler("red", self.get_red),
+                CommandHandler("3d", self.get_3d),
+            ],
+            states={
+                "black_white": [MessageHandler(Filters.photo, self.black_white_filter)],
+                "blue": [MessageHandler(Filters.photo, self.blue_filter)],
+                "red": [MessageHandler(Filters.photo, self.red_filter)],
+                "3d": [MessageHandler(Filters.photo, self.red_blue_3d_filter)],
+            },
+            fallbacks=[],
+            allow_reentry=True
+        )
+        self.dp.add_handler(conversation_handler)
+        self.logger.info('[+] command handler ready')
+
+    def start(self):
+        self.logger.info('[+] starting to get command')
+        self.dp.add_error_handler(self.error)        
+        self.updater.start_polling()
+        self.updater.idle()
+
+    def get_black_white(self, update: Update, context: CallbackContext) -> str:
+        self.logger.info('[+] geting image for the black and white filter')
+        chat_id = update.effective_chat.id
+        context.bot.send_message(chat_id=chat_id, text="send me your image !")
+        return "black_white"
+
+    def get_blue(self,update: Update, context: CallbackContext) -> str:
+        self.logger.info('[+] geting image for the blue filter')
+        chat_id = update.effective_chat.id
+        context.bot.send_message(chat_id=chat_id, text="send me your image !")
+        return "blue"
+
+    def get_red(self,update: Update, context: CallbackContext) -> str:
+        self.logger.info('[+]  geting image for the red filter')
+        chat_id = update.effective_chat.id
+        context.bot.send_message(chat_id=chat_id, text="send me your image !")
+        return "red"
+
+    def get_3d(self,update: Update, context: CallbackContext) -> str:
+        self.logger.info('[+]  geting image for red and blue filter')
+        chat_id = update.effective_chat.id
+        context.bot.send_message(chat_id=chat_id, text="send me your image !")
+        return "3d"  
+
+    def black_white_filter(self, update: Update, context: CallbackContext):
+        self.logger.info('[+] start to apply black and white filter to image')
+        try:
+            chat_id = update.effective_chat.id
+            file = context.bot.getFile(update.message.photo[-1].file_id)
+            file.download('originalPic.jpg')
+            Image.open('originalPic.jpg').convert("L").save("BW.jpg")
+            context.bot.send_message(chat_id=chat_id, text="you image is black and white now !")
+            context.bot.send_photo(chat_id=chat_id, photo=open('BW.jpg', 'rb'))
+            self.remove_image()
+        except Exception as get_image_error:
+            chat_id = update.effective_chat.id
+            context.bot.send_message(chat_id=chat_id, text=f"something is wrong try one more time {get_image_error}")
+
+    def blue_filter(self,update: Update, context: CallbackContext):
+        self.logger.info('[+] start to apply blue filter to image')       
+        try:
+            chat_id = update.effective_chat.id
+            file = context.bot.getFile(update.message.photo[-1].file_id)
+            file.download('originalPic.jpg')
+            img = self.change_pixels('originalPic.jpg', -30, -30, 100)  
+            img.save("BlueFiltered.jpg")
+            context.bot.send_photo(chat_id=chat_id, photo=open('BlueFiltered.jpg', 'rb'))
+            self.remove_image()
+        except Exception as get_image_error:
+            context.bot.send_message(chat_id=chat_id, text=f"Error: {get_image_error}")
+
+    def red_filter(self,update: Update, context: CallbackContext):
+        self.logger.info('[+] start to apply red filter to image')       
+        try:
+            chat_id = update.effective_chat.id
+            file = context.bot.getFile(update.message.photo[-1].file_id)
+            file.download('originalPic.jpg')
+            img = self.change_pixels('originalPic.jpg', 100, -30, -30)  
+            img.save("RedFiltered.jpg")
+            context.bot.send_photo(chat_id=chat_id, photo=open('RedFiltered.jpg', 'rb'))
+            self.remove_image()
+        except Exception as get_image_error:
+            context.bot.send_message(chat_id=chat_id, text=f"Error: {get_image_error}")
 
 
-def color_filter(pixel, color):
-    return pixel[0]+color[0], pixel[1]+color[1], pixel[2]+color[2]
+    def red_blue_3d_filter(self,update: Update, context: CallbackContext):
+        self.logger.info('[+] start to apply 3d filter to image')       
+        try:
+            chat_id = update.effective_chat.id
+            file = context.bot.getFile(update.message.photo[-1].file_id)
+            file.download('originalPic.jpg')
+            img = self.change_pixels('originalPic.jpg', 50, -50, 50)  
+            img.save("RedBlue3DFiltered.jpg")
+            context.bot.send_photo(chat_id=chat_id, photo=open('RedBlue3DFiltered.jpg', 'rb'))
+            self.remove_image()
+        except Exception as get_image_error:
+            context.bot.send_message(chat_id=chat_id, text=f"Error: {get_image_error}")
 
 
-def change_pixels(image_path, red, green, blue):
-    img = Image.open(image_path)
-    pixels = img.load()
-    for i in range(img.size[0]):
-        for j in range(img.size[1]):
-            # try to filter them better by find more rgb color
-            if pixels[i, j] > (230, 230, 230):
-                continue
-            else:
-                # (-150, -60, 160)
-                pixels[i, j] = color_filter(pixels[i, j], (red, green, blue))
-    return img
+    def remove_image(self,):
+        self.logger.info('[+] start to remove image')       
 
-""" ************************************** Black-White start here !***************************************"""
+        try:
+            for i in glob.glob("*.jpg"):
+                os.remove(i)
+        except Exception as delete_error:
+            self.logger.error("something is wrong file not removed")
 
-def get_black_white(bot, update):
-    chat_id = update.message.chat_id
-    bot.send_message(chat_id=chat_id, text="send me your image !")
-    return "black_white"
+    def color_filter(self,pixel, color):
+        return tuple(max(0, min(255, pixel[i] + color[i])) for i in range(3))
 
+    def change_pixels(self,image_path, red, green, blue):
+        self.logger.info('apply filter to image pixels')
+        img = Image.open(image_path)
+        pixels = img.load()
+        for i in range(img.size[0]):
+            for j in range(img.size[1]):
+                if pixels[i, j] > (230, 230, 230):
+                    continue
+                else:
+                    pixels[i, j] = self.color_filter(pixels[i, j], (red, green, blue))
+        return img
 
-def black_white_filter(bot, update):
-    # is there any way to didn't download image ?and add filter on it ?
-    try:
-        chat_id = update.message.chat_id
-        file = bot.getFile(update.message.photo[-1].file_id)
-        file.download('originalPic.jpg')
-        Image.open('originalPic.jpg').convert("L").save("BW.jpg")
-        bot.send_message(chat_id=chat_id, text="you image is black and white now !")
-        bot.send_photo(chat_id=chat_id, photo=open('BW.jpg', 'rb'))
-        remove_image()
-    except Exception as get_image_error:
-        chat_id = update.message.chat_id
-        bot.send_message(chat_id=chat_id, text=f"something is wrong try one more time {get_image_error}")
+    def start_command(self, update: Update, context: CallbackContext):
+        self.logger.info(f"User {update.effective_user.id} started the bot.", )
+        update.message.reply_text(
+            "سلام! من یک ربات تلگرام هستم که می‌توانم فیلترهای رنگی را به تصاویر شما اضافه کنم.\n"
+            "برای استفاده از قابلیت‌های من، یکی از دستورات زیر را وارد کنید:\n"
+            "/black_white - تبدیل تصویر به سیاه و سفید\n"
+            "/blue - افزودن فیلتر آبی به تصویر\n"
+            "/red - افزودن فیلتر قرمز به تصویر\n"
+            "/3d - افزودن افکت سه بعدی قرمز و آبی به تصویر\n"
+            "/help - نمایش این راهنما"
+        )
 
+    def help_command(self, update: Update, context: CallbackContext):
+        self.logger.info(f"User {update.effective_user.id} asked for help.", ) 
+        update.message.reply_text(
+            "شما می‌توانید با ارسال تصویر و انتخاب یکی از دستورات زیر، فیلتر مورد نظر خود را اعمال کنید:\n"
+            "/black_white - برای تبدیل تصویر به سیاه و سفید\n"
+            "/blue - برای افزودن فیلتر آبی به تصویر\n"
+            "/red - برای افزودن فیلتر قرمز به تصویر\n"
+            "/3d - برای افزودن افکت سه بعدی قرمز و آبی\n"
+            "برای شروع، فقط تصویری را ارسال کنید و دستور مربوطه را انتخاب نمایید."
+        )
 
-""" ************************************** Black-White end here !***************************************"""
-
-""" ************************************** blue start here !***************************************"""
-
-
-def get_blue(bot, update):
-    chat_id = update.message.chat_id
-    bot.send_message(chat_id=chat_id, text="send me your image !")
-    return "blue"
-
-
-def blue_filter(bot, update):
-    try:
-        chat_id = update.message.chat_id
-        file = bot.getFile(update.message.photo[-1].file_id)
-        file.download('originalBluePic.jpg')
-        # -150,-60,160
-        img = change_pixels('originalBluePic.jpg', -150, -80, 130)
-        img.save("blueImage.jpg")
-        bot.send_message(chat_id=chat_id, text="blue filter is add to your image")
-        bot.send_photo(chat_id=chat_id, photo=open('blueImage.jpg', 'rb'))
-        remove_image()
-    except Exception as get_image_error:
-        chat_id = update.message.chat_id
-        bot.send_message(chat_id=chat_id, text=f"something is wrong try one more time {get_image_error}")
-
-
-""" *************************************** blue is end here***************************************"""
-
-
-""" ***************************************red is start here***************************************"""
-
-
-def get_red(bot, update):
-    chat_id = update.message.chat_id
-    bot.send_message(chat_id=chat_id, text="send me your image !")
-    return "red"
-
-
-def red_filter(bot, update):
-    try:
-        chat_id = update.message.chat_id
-        file = bot.getFile(update.message.photo[-1].file_id)
-        file.download('originalRedPic.jpg')
-        # 75,-90,-110
-        img = change_pixels('originalRedPic.jpg', 130, -90, -90)
-        img.save("redImage.jpg")
-        bot.send_message(chat_id=chat_id, text="red filter is add")
-        bot.send_photo(chat_id=chat_id, photo=open('redImage.jpg', 'rb'))
-        remove_image()
-    except Exception as get_image_error:
-        chat_id = update.message.chat_id
-        bot.send_message(chat_id=chat_id, text=f"something is wrong try one more time {get_image_error}")
-
-
-""" ***************************************red is end ***************************************"""
-
-def main():
-    updater = Updater('token is here !')
-    db = updater.dispatcher
-    conversation_handler = ConversationHandler(
-        entry_points=[
-            CommandHandler("black_white", get_black_white),
-            CommandHandler("blue", get_blue),
-            CommandHandler("red", get_red),
-
-        ],
-
-        states={
-            "black_white": [MessageHandler(Filters.photo, black_white_filter)],
-            "blue": [MessageHandler(Filters.photo, blue_filter)],
-            "red": [MessageHandler(Filters.photo, red_filter)],
-        },
-        fallbacks=[
-            MessageHandler(Filters.photo, black_white_filter),
-            CommandHandler("black_white", get_black_white),
-            MessageHandler(Filters.photo, blue_filter),
-            CommandHandler("blue", get_blue),
-            MessageHandler(Filters.photo, red_filter),
-            CommandHandler("red", get_red),
-
-        ],
-        allow_reentry=True,
-        per_user=True,
-        conversation_timeout=3600,
-        name="test"
-    )
-    db.add_handler(conversation_handler)
-    updater.start_polling()
-    updater.idle()
+    def error(self, update, context):
+        """Log Errors caused by Updates."""
+        self.logger.warning(f'Update "{update}" caused error "{context.error}"',  )
 
 
 if __name__ == '__main__':
-    main()
+    load_dotenv()  
+    TOKEN = os.getenv("TELEGRAM_TOKEN")
+    color_filter_bot = ColorFilterBot(TOKEN)
+    color_filter_bot.start()
---
+++ readme.en.md
@@ -0,0 +1,39 @@
+# Telegram Color Filter Bot
+
+The Telegram Color Filter Bot is a bot that allows you to apply various color filters to your images. This bot can convert your images to black and white, add color filters like blue or red, and even create a red and blue 3D effect.
+
+## How to Use
+
+To use this bot, first search for it in Telegram and then start chatting. After starting the chat, you can use the following commands:
+
+- `/start`: Begin interacting with the bot and receive initial instructions.
+- `/help`: Get guidance on how to use the bot.
+- `/black_white`: Convert the sent image to black and white.
+- `/blue`: Add a blue filter to the image.
+- `/red`: Add a red filter to the image.
+- `/3d`: Apply a red and blue 3D effect.
+
+## Installation and Setup
+
+To set up this bot on your server or development environment, follow the steps below:
+
+1. Ensure `python3` and `pip` are installed on your system.
+2. Install the required libraries using the `requirements.txt` file:
+
+```bash
+pip install -r requirements.txt
+```
+3. Create a `.env` file at the root of the project and add your Telegram bot token like so:
+
+```bash
+TELEGRAM_TOKEN=your_telegram_bot_token_here
+```
+4. Run the bot script with the following command:
+
+```bash 
+python bot.py
+```
+
+## Contributing
+
+Contributions to improve this bot are welcome. Please create an issue first to discuss proposed changes or improvements.
